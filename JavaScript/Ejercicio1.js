047637304dcedeb11d9e94158c5fc91a43b71111
avazquez99 <vazqueza193@gmail.com>
2023-05-03 18:53:19 +0100

    Ejercicio 1

---
+++ JavaScript/Ejercicio1.js
@@ -0,0 +1,12 @@
+////////// EJERCICIO 1 //////////
+
+// Escribir un algoritmo en el cual se consulte al usuario que ingrese ¿cómo está el día de 
+// hoy? (soleado, nublado, lloviendo). A continuación, mostrar por pantalla un mensaje que 
+// indique “El día de hoy está ...”, completando el mensaje con el dato que ingresó el usuario. 
+
+/////////////////////////////////
+
+let estadoDia = prompt("¿Cómo está el día de hoy? (soleado, nublado, lloviendo)");
+
+console.log("El día de hoy está " + estadoDia);
+
